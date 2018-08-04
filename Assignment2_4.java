7063df1b188d9d591a83ba8e01c1fc5dcaa09ab3
Adam Kim <akim456@gmail.com>
2018-08-03 23:49:38 -0400

    removed unused imports

--- Assignment2_4.java
+++ Assignment2_4.java
@@ -8,13 +8,10 @@
 
 package week6_A2_BASIC_INPUT_AND_OUTPUT2;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
