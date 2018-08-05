e92478b128269d53ca948cda4efe4aaf69ef07c8
Adam Kim <akim456@gmail.com>
2018-08-04 22:42:31 -0400

    finished 6_2_4to5

--- Assignment2_4to5.java
+++ Assignment2_4to5.java
@@ -18,29 +18,29 @@ import java.util.Scanner;
 public class Assignment2_4to5 {
 
 	public static void main(String[] args) throws IOException {	
-//--QUESTION 4 PRINTING TOTAL VALUES OF 3 RANDOM SPACED OUT INTEGERS FROM A FILE
+		//QUESTION 4 PRINTING TOTAL VALUES OF 3 RANDOM SPACED OUT INTEGERS FROM A FILE
 		addThree();
 		
-//--QUESTION 5 
+		//QUESTION 5 (USER CHOOSES FROM 3 FILES, ADD AND RETURN THE RANDOM SPACED INTEGERS
 		pickOne();
 	}
 	
-	//--QUESTION 4 METHOD (SCANNER ADDED 3 INTEGERS, SPLIT SPACED FROM FILE,
+//--QUESTION 4 METHOD (SCANNER ADDED 3 INTEGERS, SPLIT SPACED FROM FILE,
 //--LOOPED TO ADD INTO ARRAYLIST THEN ADDED ALL ELEMENTS OF THE ARRAYLIST
 	private static void addThree() throws FileNotFoundException {
 		System.out.println("QUESTION 4--------------------------------------------------------");
 		
 		//INITIALIZING VARIABLES, SCANNER, ARRAY, ARRAYLIST
 		File readin = new File("C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
-													+ "\\src\\week6_A2_BASIC_INPUT_AND_OUTPUT2\\FILE_BANK\\num1.txt");
-		Scanner scanner = new Scanner(readin);
-		List<Integer> arr = new ArrayList<>();
+												   + "\\src\\week6_A2_BASIC_INPUT_AND_OUTPUT2\\FILE_BANK\\num1.txt");
+		Scanner Q4Scanner = new Scanner(readin);
 		String[] data;
+		List<Integer> arr = new ArrayList<>();
 		int total = 0;
 
 		//USING WHILE LOOP TO SPLIT SPACES AND ADD INTEGERS INTO AN ARRAY
-		while (scanner.hasNextLine()) {
-			data = (scanner.next().split(" "));
+		while (Q4Scanner.hasNextLine()) {
+			data = (Q4Scanner.next().split(" "));
 			for (int i = 0; i < data.length; i++) {
 				arr.add(Integer.parseInt(data[i]));
 			}
@@ -56,107 +56,106 @@ public class Assignment2_4to5 {
 		System.out.println("If we add them, the sum is : " + total);
 
 		//CLOSES SCANNER
-		scanner.close();
+		Q4Scanner.close();
 	}
 	
 //--QUESTION 5 (PICKONE) 
 	private static void pickOne() throws FileNotFoundException {
 		System.out.println("\r\nQUESTION 5--------------------------------------------------------");
 		
-		String pathQ5 = "C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
-													+ "\\src\\week6_A2_BASIC_INPUT_AND_OUTPUT2\\FILE_BANK\\";
-				
+		//INSTRUCTIONS FOR THE USER
 		System.out.println("Hello, which file would you like to read and get total sum for?");
 		System.out.println("A. num2.txt \r\nB. num3.txt \r\nC. num4.txt\r\n(Choose a letter)");
 			
-		Scanner scannerQ5 = new Scanner(System.in);
-		String choice = scannerQ5.next();
+		//INITIALIZE VARIABLES BEING USED FOR THE SWITCH CASE STATEMENT
+		String Q5path = "C:\\Users\\akim4\\Google Drive\\CODE\\JAVAPROGRAMS\\PERSCHOLAS PLATFORM JD WORKSPACE"
+													+ "\\src\\week6_A2_BASIC_INPUT_AND_OUTPUT2\\FILE_BANK\\";
+		Scanner Q5scanner = new Scanner(System.in);
+		String choice = Q5scanner.next();
+		String[] Q5data;
+		List<Integer> Q5arr = new ArrayList<>();
+		int Q5total = 0;
 		
 		
+		//SWITCH STATEMENT TAKES USER'S CHOICE AND SELECTS FILE ACCORDINGLY
+		//DO NOT GET OVERWHELMED WITH THIS SWITCH CASE STATEMENT AS
+		//ALL CHOICES ARE THE SAME EXCEPT VARIABLE NAME DIFFERENCES AND FILE PATH
 		switch (choice.toLowerCase()) {
-			case "a" :	File fileQ5 = new File(pathQ51 + "num2.txt");
-				
-						Scanner fileScannerQ51 = new Scanner(fileQ51);
-						List<Integer> arrQ51 = new ArrayList<>();
-						String[] dataQ51;
-						int totalQ51 = 0;
-			
+			case "a" :	//INITIALIZE ABSOLUTE FILE ADDRESS AND FILE SCANNER
+						File Q5file2 = new File(Q5path + "num2.txt");
+						Scanner Q51FileScanner2 = new Scanner(Q5file2);
+									
 						//USING WHILE LOOP TO SPLIT SPACES AND ADD INTEGERS INTO AN ARRAY
-						while (fileScannerQ51.hasNextLine()) {
-							dataQ51 = (fileScannerQ51.next().split(" "));
-							for (int i = 0; i < dataQ51.length; i++) {
-								arrQ51.add(Integer.parseInt(dataQ51[i]));
+						while (Q51FileScanner2.hasNextLine()) {
+							Q5data = (Q51FileScanner2.next().split(" "));
+							for (int i = 0; i < Q5data.length; i++) {
+								Q5arr.add(Integer.parseInt(Q5data[i]));
 							}
 						}
 						
-						for (int k = 0; k < arrQ51.size(); k++) {
-							totalQ51 += arrQ51.get(k);
+						//USING FOR LOOP TO ADD EACH ELEMENT IN THE ARRAY 
+						for (int k = 0; k < Q5arr.size(); k++) {
+							Q5total += Q5arr.get(k);
 						}
 			
 						//PRINTING OUT RESULTS TO USER
-						System.out.println("File consisted of random spaces between 3 numbers : " + arrQ51);
-						System.out.println("They add up to : " + totalQ51);
+						System.out.println("File consisted of random spaces between 3 numbers : " + Q5arr);
+						System.out.println("They add up to : " + Q5total);
 			
 						//CLOSES SCANNER
-						fileScannerQ51.close();
-						scannerQ51.close();
+						Q51FileScanner2.close();
 						break;
 				
-			case "b" : 	File fileQ52 = new File(pathQ52 + "num3.txt");
-			
-						Scanner fileScannerQ52 = new Scanner(fileQ52);
-						List<Integer> arrQ52 = new ArrayList<>();
-						String[] dataQ52;
-						int totalQ52 = 0;
-			
+			case "b" : 	//INITIALIZE ABSOLUTE FILE ADDRESS AND FILE SCANNER
+						File Q5file3 = new File(Q5path + "num3.txt");
+						Scanner Q51FileScanner3 = new Scanner(Q5file3);
+									
 						//USING WHILE LOOP TO SPLIT SPACES AND ADD INTEGERS INTO AN ARRAY
-						while (fileScannerQ52.hasNextLine()) {
-							dataQ52 = (fileScannerQ52.next().split(" "));
-							for (int i = 0; i < dataQ52.length; i++) {
-								arrQ52.add(Integer.parseInt(dataQ52[i]));
+						while (Q51FileScanner3.hasNextLine()) {
+							Q5data = (Q51FileScanner3.next().split(" "));
+							for (int i = 0; i < Q5data.length; i++) {
+								Q5arr.add(Integer.parseInt(Q5data[i]));
 							}
 						}
 						
-						for (int k = 0; k < arrQ52.size(); k++) {
-							totalQ52 += arrQ52.get(k);
+						//USING FOR LOOP TO ADD EACH ELEMENT IN THE ARRAY 
+						for (int k = 0; k < Q5arr.size(); k++) {
+							Q5total += Q5arr.get(k);
 						}
 			
 						//PRINTING OUT RESULTS TO USER
-						System.out.println("File consisted of random spaces between 3 numbers : " + arrQ52);
-						System.out.println("They add up to : " + totalQ52);
+						System.out.println("File consisted of random spaces between 3 numbers : " + Q5arr);
+						System.out.println("They add up to : " + Q5total);
 			
 						//CLOSES SCANNER
-						fileScannerQ52.close();
-						scannerQ52.close();
+						Q51FileScanner3.close();
 						break;
 						
-			case "c" :	File fileQ53 = new File(pathQ53 + "num4.txt");
-						
-						Scanner fileScannerQ53 = new Scanner(fileQ53);
-						List<Integer> arrQ53 = new ArrayList<>();
-						String[] dataQ53;
-						int totalQ53 = 0;
-			
+			case "c" :	//INITIALIZE ABSOLUTE FILE ADDRESS AND FILE SCANNER
+						File Q5file4 = new File(Q5path + "num4.txt");
+						Scanner Q51FileScanner4 = new Scanner(Q5file4);
+									
 						//USING WHILE LOOP TO SPLIT SPACES AND ADD INTEGERS INTO AN ARRAY
-						while (fileScannerQ53.hasNextLine()) {
-							dataQ53 = (fileScannerQ53.next().split(" "));
-							for (int i = 0; i < dataQ53.length; i++) {
-								arrQ53.add(Integer.parseInt(dataQ53[i]));
+						while (Q51FileScanner4.hasNextLine()) {
+							Q5data = (Q51FileScanner4.next().split(" "));
+							for (int i = 0; i < Q5data.length; i++) {
+								Q5arr.add(Integer.parseInt(Q5data[i]));
 							}
 						}
 						
-						for (int k = 0; k < arrQ53.size(); k++) {
-							totalQ53 += arrQ53.get(k);
+						//USING FOR LOOP TO ADD EACH ELEMENT IN THE ARRAY 
+						for (int k = 0; k < Q5arr.size(); k++) {
+							Q5total += Q5arr.get(k);
 						}
 			
 						//PRINTING OUT RESULTS TO USER
-						System.out.println("File consisted of random spaces between 3 numbers : " + arrQ53);
-						System.out.println("They add up to : " + totalQ53);
+						System.out.println("File consisted of random spaces between 3 numbers : " + Q5arr);
+						System.out.println("They add up to : " + Q5total);
 			
 						//CLOSES SCANNER
-						fileScannerQ53.close();
-						scannerQ53.close();
-						break;		
+						Q51FileScanner4.close();
+						break;	
 		}
+		Q5scanner.close();
 	}
 }
